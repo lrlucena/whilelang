5de38bbcc1f9d0c56008566e3a98ceea312bb609
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-01-10 17:43:20 -0200

    Update MyListener.scala

--- src/whilelang/interpreter/MyListener.scala
+++ src/whilelang/interpreter/MyListener.scala
@@ -1,95 +1,74 @@
-package whilelang
+package whilelang.interpreter
 
-import whilelang.{ WhilelangParser => C } 
-import whilelang.Language._
-import scala.collection.JavaConverters._
+import scala.collection.JavaConverters.asScalaBufferConverter
+import whilelang.interpreter.Language._
+import whilelang.parser.{ Antlr2Scala, WhilelangBaseListener, WhilelangParser => C }
 
-class MyListener extends WhilelangBaseListener with Antlr2Scala {
+class MyListener extends WhilelangBaseListener with Antlr2Scala[Any] {
   var _program: Program = _
   def program = _program
 
-  override def exitProgram(ctx: C.ProgramContext) {
-    _program = Program(ctx.seqStatement.value[List[Statement]])
-  }
+  override def exitProgram(ctx: C.ProgramContext) =
+    _program = Program(ctx.seqStatement.value)
 
-  override def exitSeqStatement(ctx: C.SeqStatementContext) {
-    ctx.value = ctx.statement().asScala.toList.map { _.value[Statement] }
-  }
+  override def exitSeqStatement(ctx: C.SeqStatementContext) =
+    ctx.value = SeqStatement(ctx.statement().asScala.toList.map { _.value[Statement] })
 
-  override def exitAttrib(ctx: C.AttribContext) {
+  override def exitAttrib(ctx: C.AttribContext) =
     ctx.value = Attrib(ctx.ID.text, ctx.expression.value)
-  }
 
-  override def exitSkip(ctx: C.SkipContext) {
+  override def exitSkip(ctx: C.SkipContext) =
     ctx.value = Skip
-  }
 
-  override def exitIf(ctx: C.IfContext) = {
+  override def exitIf(ctx: C.IfContext) =
     ctx.value = If(ctx.bool.value, ctx.statement(0).value, ctx.statement(1).value)
-  }
 
-  override def exitWhile(ctx: C.WhileContext) {
+  override def exitWhile(ctx: C.WhileContext) =
     ctx.value = While(ctx.bool.value, ctx.statement.value)
-  }
 
-  override def exitPrint(ctx: C.PrintContext) = {
+  override def exitPrint(ctx: C.PrintContext) =
     ctx.value = Print(ctx.Text.text.drop(1).dropRight(1))
-  } 
 
-  override def exitWrite(ctx: C.WriteContext) = {
-    ctx.value = Write(ctx.expression.value)
-  }
+  override def exitWrite(ctx: C.WriteContext) =
+    ctx.value = Write(ctx.expression.value[Expression])
 
-  override def exitBlock(ctx: C.BlockContext) = {
-    ctx.value = Block(ctx.seqStatement.value)
-  }
+  override def exitBlock(ctx: C.BlockContext) =
+    ctx.value = ctx.seqStatement.value
 
-  override def exitRead(ctx: C.ReadContext) = {
+  override def exitRead(ctx: C.ReadContext) =
     ctx.value = Read
-  }
 
-  override def exitId(ctx: C.IdContext) {
+  override def exitId(ctx: C.IdContext) =
     ctx.value = Id(ctx.ID.text)
-  }
 
-  override def exitExpParen(ctx: C.ExpParenContext) {
+  override def exitExpParen(ctx: C.ExpParenContext) =
     ctx.value = ctx.expression.value
-  }
 
-  override def exitInt(ctx: C.IntContext) {
+  override def exitInt(ctx: C.IntContext) =
     ctx.value = Integer(ctx.text.toInt)
-  }
 
-  override def exitBinOp(ctx: C.BinOpContext) {
-    val exp = ctx(1).text match {
-      case "*" => ExpMult
-      case "-" => ExpSub
-      case _   => ExpSum
-    }
-    ctx.value = exp(ctx.expression(0).value, ctx.expression(1).value)
-  }
+  override def exitBinOp(ctx: C.BinOpContext) =
+    ctx.value = (ctx(1).text match {
+      case "*"     => ExpMult
+      case "-"     => ExpSub
+      case "+" | _ => ExpSum
+    })(ctx.expression(0).value, ctx.expression(1).value)
 
-  override def exitNot(ctx: C.NotContext) {
+  override def exitNot(ctx: C.NotContext) =
     ctx.value = Not(ctx.bool.value)
-  }
 
-  override def exitBoolean(ctx: C.BooleanContext) = {
+  override def exitBoolean(ctx: C.BooleanContext) =
     ctx.value = Boole(ctx.text == "true")
-  }
 
-  override def exitAnd(ctx: C.AndContext) {
+  override def exitAnd(ctx: C.AndContext) =
     ctx.value = And(ctx.bool(0).value, ctx.bool(1).value)
-  }
 
-  override def exitBoolParen(ctx: C.BoolParenContext) {
+  override def exitBoolParen(ctx: C.BoolParenContext) =
     ctx.value = ctx.bool.value
-  }
 
-  override def exitRelOp(ctx: C.RelOpContext) {
-    val exp = ctx(1).text match {
-      case "="  => ExpEqual
-      case _    => ExpLessOrEqualThan
-    }
-    ctx.value = exp(ctx.expression(0).value, ctx.expression(1).value)
-  }
+  override def exitRelOp(ctx: C.RelOpContext) =
+    ctx.value = (ctx(1).text match {
+      case "="      => ExpEqual
+      case "<=" | _ => ExpLessOrEqualThan
+    })(ctx.expression(0).value, ctx.expression(1).value)
 }
