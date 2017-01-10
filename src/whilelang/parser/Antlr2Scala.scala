2e772db6811aebf4f576d041a7ed75584b5b342a
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-01-10 17:45:20 -0200

    Update Antlr2Scala.scala

--- src/whilelang/parser/Antlr2Scala.scala
+++ src/whilelang/parser/Antlr2Scala.scala
@@ -1,13 +1,13 @@
-package whilelang
+package whilelang.parser
 
-import org.antlr.v4.runtime.tree.{ParseTree, ParseTreeProperty}
+import org.antlr.v4.runtime.tree.{ ParseTree, ParseTreeProperty }
 
-trait Antlr2Scala {
-  protected val values = new ParseTreeProperty[Any]
+trait Antlr2Scala[T] {
+  protected val values = new ParseTreeProperty[T]
   protected implicit class tree2scala(tree: ParseTree) {
     def apply(i: Int) = tree.getChild(i)
     def text = tree.getText
-    def value[T]: T = values.get(tree).asInstanceOf[T]
-    def value_=(v: Any) = values.put(tree, v)
+    def value[E]: E = values.get(tree).asInstanceOf[E]
+    def value_=(v: T) = values.put(tree, v)
   }
 }
