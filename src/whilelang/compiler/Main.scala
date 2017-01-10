94d52b3693ee2a0395f8ff86f90c500dce667c87
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-01-10 17:33:54 -0200

    Update Main.scala

--- src/whilelang/compiler/Main.scala
+++ src/whilelang/compiler/Main.scala
@@ -1,1 +1,14 @@
+package whilelang.compiler
 
+import java.io.FileNotFoundException
+import scala.util.{ Failure, Success, Try }
+import whilelang.parser.Walker.walk
+
+object Main extends App {
+  implicit val listener = new Compiler()
+  Try(io.Source.fromFile(args(0)).getLines.mkString("\n")).flatMap(walk) match {
+    case Success(_)                        => println(listener.program)
+    case Failure(e: FileNotFoundException) => println("File not found")
+    case Failure(e)                        => println("Error: " + e.printStackTrace())
+  }
+}
